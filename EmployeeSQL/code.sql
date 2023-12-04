5f1d9376895d188424df539db826cebe0ac242b1
sallyca530 <sally.ca530@gmail.com>
2023-12-04 12:32:19 -0800

    Official ERD schema

--- EmployeeSQL/code.sql
+++ EmployeeSQL/code.sql
@@ -1,41 +1,41 @@
 Schema -
--- Title
--- -
--- TitleID PK int
--- title_id string FK >- Employees.emp_title_id
--- title string
+Title
+-
+TitleID PK int
+title_id Varchar(5) FK >- Employees.emp_title_id
+title Varchar(30) 
 
--- Salaries
--- -
--- SalaryID PK int
--- emp_no int  FK >- Employees.emp_no
--- salary int
+Salaries
+-
+SalaryID PK int
+emp_no int  FK >- Employees.emp_no
+salary int
 
--- Employees
--- -
--- EmployeeID PK int
--- emp_no int
--- emp_title_id string 
--- birth_date string
--- first_name string 
--- last_name string
--- sex string
--- hire_date string
+Employees
+-
+EmployeeID PK int
+emp_no int
+emp_title_id Varchar(5) 
+birth_date DATE
+first_name Varchar(30)
+last_name Varchar(30)
+sex Varchar(2)
+hire_date DATE
 
--- Department_Manager
--- -
--- DeptManagerID PK int
--- dept_no string FK >- Department_Employees.dept_no
--- emp_no int FK >- Employees.emp_no
+Department_Manager
+-
+DeptManagerID PK int
+dept_no Varchar(4) FK -< Department_Employees.dept_no
+emp_no int FK >- Employees.emp_no
 
--- Department_Employees
--- -
--- DeptEmpID PK int
--- emp_no int FK >- Employees.emp_no 
--- dept_no string
+Department_Employees
+-
+DeptEmpID PK int
+emp_no int FK >- Employees.emp_no 
+dept_no string
 
--- Departments
--- -
--- DepartmentsID PK int
--- dept_no string FK >- Department_Manager.dept_no 
--- dept_name string
+Departments
+-
+DepartmentsID PK int
+dept_no Varchar(4) FK -< Department_Manager.dept_no 
+dept_name Varchar(30)
