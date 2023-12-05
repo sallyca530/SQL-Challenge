9d3f48dfd9d7b44c73b9929638748cd222b2ca43
sallyca530 <sally.ca530@gmail.com>
2023-12-04 17:32:21 -0800

    queries 7-8

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -63,8 +63,19 @@ where departments.dept_name = 'Sales';
 --------------------------------------------------------------------------------------------------
 -- 7. List each employee in the Sales and Development departments, including their 
 -- employee number, last name, first name, and department name.
-
+select employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+from employees 
+inner join dept_emp 
+on employees.emp_no = dept_emp.emp_no
+join departments
+on dept_emp.dept_no = departments.dept_no
+where departments.dept_name = 'Sales' or departments.dept_name = 'Development';
 
 --------------------------------------------------------------------------------------------------
 -- 8. List the frequency counts, in descending order, of all the employee last names 
 -- (that is, how many employees share each last name)
+select last_name, count(last_name) as Frequency 
+from employees 
+group by last_name
+order by frequency desc;
+
